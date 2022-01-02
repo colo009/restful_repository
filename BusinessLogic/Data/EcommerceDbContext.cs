27839a949d684fb6cf88c30054a2b84a56c688ea
colo009 <vergil009009@gmail.com>
2022-01-02 20:40:54 -0300

    1st migration - 24

---
+++ BusinessLogic/Data/EcommerceDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Data
+{
+    public class EcommerceDbContext : DbContext
+    {
+        public EcommerceDbContext(DbContextOptions<EcommerceDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Producto> Productos { set; get; }
+        public DbSet<Marca> Marcas { set; get; }
+        public DbSet<Categoria> Categorias { set; get; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
